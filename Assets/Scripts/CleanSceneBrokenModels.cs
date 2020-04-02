236596ac3b22bf8846e538d103aecd7ab6c4dbe5
OrrinBlake <orrinblake@gmail.com>
2020-04-02 14:46:33 +0100

    destroy broken models

---
+++ Assets/Scripts/CleanSceneBrokenModels.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanSceneBrokenModels : MonoBehaviour
+{
+    public GameObject _wholeModel;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        StartCoroutine(DestroyObject());
+    }
+
+    private IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(3);
+        
+        Destroy(gameObject);
+        
+        yield return new WaitForSeconds(2);
+
+        Instantiate(_wholeModel, gameObject.transform.position, gameObject.transform.rotation);
+    }
+}
