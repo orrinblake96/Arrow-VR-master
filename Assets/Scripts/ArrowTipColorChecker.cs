57dd924b9b36f88956130b83a7830835f674aad2
OrrinBlake <orrinblake@gmail.com>
2020-04-06 21:04:09 +0100

    Tip color change, Check color comparison & simple UI
    - Arrow tip changes color with click of “a” button.
    - Check if tip color == cube color then destroy.
    - Simple UI for testing purposes.
    - Scene resets using “x” button for ease of testing.

--- Assets/Scripts/ArrowTipColorChecker.cs
+++ Assets/Scripts/ArrowTipColorChecker.cs
@@ -1,54 +1,65 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ArrowTipColorChecker : MonoBehaviour
 {
+    public Material[] cubeColors;
+    public GameObject newCube;
+    
     private MeshRenderer _tipColor;
+    private MeshRenderer _cubeColor;
     private Text _arrowInfoText;
     private Text _colorMatchText;
-    private Text _enemyDestroyedText;
+    private int _currentMaterial = 0;
 
     private void Awake()
     {
         _tipColor = GetComponent<MeshRenderer>();
         _arrowInfoText = GameObject.Find("Canvas/ArrowInfoText").GetComponent<Text>();
         _colorMatchText = GameObject.Find("Canvas/ColorMatch").GetComponent<Text>();
-        _enemyDestroyedText = GameObject.Find("Canvas/EnemyDestroyed").GetComponent<Text>();
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _arrowInfoText.text = "Arrow Color: " + _tipColor.material.color.ToString();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (_tipColor.material.color.r == 1.0f)
-        {
-            _colorMatchText.text = "Color Matched: true";
-        }
-        else
-        {
-            _colorMatchText.text = "Color Matched: false";
-        }
-
         if (OVRInput.GetDown(OVRInput.Button.One))
         {
-//            _enemyDestroyedText.text = "Enemy Dead: True";
+            _tipColor.material = cubeColors[(_currentMaterial++) % 3];
+            _arrowInfoText.text = "Arrow Color: " + _tipColor.material.color.ToString();
+        }
+        
+        //Reset scene for quick testing
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("RedEnemy"))
+        //Check if cubes color matches tip of arrow
+        _cubeColor = other.gameObject.GetComponent<MeshRenderer>();
+        if ((_cubeColor.material.color.r == _tipColor.material.color.r) && 
+            (_cubeColor.material.color.g == _tipColor.material.color.g) && 
+            (_cubeColor.material.color.b == _tipColor.material.color.b))
         {
-            _enemyDestroyedText.text = "Enemy Dead: True";
+//            Transform cubePosition = other.gameObject.transform;
+            _colorMatchText.text = "Color Matched: true";
             Destroy(other.gameObject);
+//            Instantiate(newCube, cubePosition.transform.position, cubePosition.transform.rotation);
+        }
+        else
+        {
+            _colorMatchText.text = "Color Matched: false";
         }
     }
 }
