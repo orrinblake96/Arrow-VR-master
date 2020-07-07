70c593c227cbefee879e885d2c370732b57b0891
OrrinBlake <orrinblake@gmail.com>
2020-07-07 11:21:01 +0100

    Target Socring & baked lighting

--- Assets/Scripts/TargetShooterMiniGame/TargetHealth.cs
+++ Assets/Scripts/TargetShooterMiniGame/TargetHealth.cs
@@ -1,10 +1,18 @@
-﻿using Crate;
+﻿using System;
+using Crate;
 using UnityEngine;
 
 namespace TargetShooterMiniGame
 {
     public class TargetHealth : MonoBehaviour, IDamageable
     {
+        private TargetScore _targetScore;
+
+        private void Start()
+        {
+            _targetScore = GameObject.Find("ScorenumberTMP").GetComponent<TargetScore>();
+        }
+
         public void Damage(int amount)
         {
             DestroyCrate();
@@ -12,6 +20,8 @@ namespace TargetShooterMiniGame
 
         private void DestroyCrate()
         {
+            _targetScore.IncreaseCurrentScore();
+            print("Score");
             Destroy(gameObject);
         }
     }
