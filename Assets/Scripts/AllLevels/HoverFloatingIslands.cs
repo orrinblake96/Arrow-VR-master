ae7fb5026a2e41d6795be66bd7fba5df2841f240
OrrinBlake <orrinblake@gmail.com>
2020-09-09 22:12:59 +0100

    New monkey cottage, lighthouse and made island float

---
+++ Assets/Scripts/AllLevels/HoverFloatingIslands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace AllLevels
+{
+    public class HoverFloatingIslands : MonoBehaviour
+    {
+        private void Start()
+        {
+            LeanTween.moveLocalY(gameObject, 4f, 8f).setLoopPingPong().setEase(LeanTweenType.easeInOutQuad);
+        }
+    }
+}
