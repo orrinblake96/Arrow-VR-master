e6b2658c5acd63b092ffc75555cb64678c2feac3
OrrinBlake <orrinblake@gmail.com>
2020-09-15 11:28:38 +0100

    BG music doesn't restart

---
+++ Assets/Scripts/Audio/FmodBackgroundMusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Audio
+{
+    public class FmodBackgroundMusicManager : MonoBehaviour
+    {
+        private static FmodBackgroundMusicManager _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+            
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
