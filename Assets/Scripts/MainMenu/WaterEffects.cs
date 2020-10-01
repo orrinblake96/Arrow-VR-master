f7e6393f1ddb44c2e4f6dd63e98e20ab70ddf069
OrrinBlake <orrinblake@gmail.com>
2020-10-01 14:20:02 +0100

    Water particle effect when hit

---
+++ Assets/Scripts/MainMenu/WaterEffects.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MainMenu
+{
+    public class WaterEffects : MonoBehaviour
+    {
+        [SerializeField] private GameObject waterSplashParticleEffect;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name != "Tip") return;
+            GameObject waterSplash = Instantiate(waterSplashParticleEffect, other.transform.position, Quaternion.Euler(-90, 0, 0));
+            Destroy(waterSplash, 1f);
+        }
+    }
+}
