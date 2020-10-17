500153ae3c5c6c2e693bc5ae7ffdfe45f1a7c2fc
OrrinBlake <orrinblake@gmail.com>
2020-10-17 18:28:24 +0100

    Arrow, target model with materials, Target score effects, Game manager and logic
    - Create new arrow for game-mode.
    
    - Create targets, materials and effects for scores.
    
    - Create a game-manager to handle targets and target scripts to handle
    damage and effects.

---
+++ Assets/Scripts/AccuracyMiniGame/AccuracyGameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AccuracyMiniGame
+{
+    public class AccuracyGameManager : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> easyTargetsList;
+        [SerializeField] private List<GameObject> mediumTargetsList;
+        [SerializeField] private List<GameObject> hardTargetsList;
+
+        private List<GameObject> _currentTargetList;
+        private int _currentTargetListIndex = 0;
+        private GameObject _nextPosition;
+        private Transform _playerPosition;
+
+        private void Start()
+        {
+            _playerPosition = GameObject.Find("OVRCameraRig").transform;
+            _currentTargetList = easyTargetsList;
+        }
+
+        public void DisplayNextTarget(GameObject target)
+        {
+            if (_currentTargetList.Count <= 0)
+            {
+                _currentTargetListIndex++;
+                GetNextTargetList(target);
+                return;
+            }
+            
+            _nextPosition = GetNextTarget();
+
+            target.transform.position = _nextPosition.transform.position;
+            target.transform.rotation = _nextPosition.transform.rotation;
+
+            StartCoroutine(ActivateTarget(target));
+
+            _currentTargetList.Remove(_nextPosition);
+        }
+
+        private GameObject GetNextTarget()
+        {
+            return _currentTargetList[Random.Range(0, _currentTargetList.Count)];
+        }
+
+        private void GetNextTargetList(GameObject target)
+        {
+            switch (_currentTargetListIndex)
+            {
+                case 1:
+                    _currentTargetList = mediumTargetsList;
+                    DisplayNextTarget(target);
+                    break;
+                case 2:
+                    _currentTargetList = hardTargetsList;
+                    DisplayNextTarget(target);
+                    break;
+                case 3:
+                    Debug.Log("Done");
+                    break;
+            }
+        }
+
+        private static IEnumerator ActivateTarget(GameObject target)
+        {
+            yield return new WaitForSeconds(1f);
+            target.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/AccuracyMiniGame/AccuracyTargetLogic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Crate;
+using UnityEngine;
+
+namespace AccuracyMiniGame
+{
+    public class AccuracyTargetLogic : MonoBehaviour, IDamageable
+    {
+        [Header("Hit Info")]
+        [SerializeField] private int hitAmount;
+
+        [Header("Target Effects")] [SerializeField]
+        private GameObject scoreNumberEffect; 
+        
+
+        private AccuracyGameManager _accuracyGameManager;
+
+        private void Start()
+        {
+            _accuracyGameManager = GameObject.Find("GM").GetComponent<AccuracyGameManager>();
+        }
+
+//        private void Update()
+//        {
+//            if(Input.GetKeyDown(KeyCode.Space)) Damage(10);
+//        }
+
+        public void Damage(int amount)
+        {
+            Instantiate(scoreNumberEffect, transform.position, transform.rotation);
+            _accuracyGameManager.DisplayNextTarget(transform.parent.gameObject);
+            
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
