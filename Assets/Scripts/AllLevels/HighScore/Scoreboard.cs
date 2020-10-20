4468c78b073a7141b00f1136e76607b823991932
OrrinBlake <orrinblake@gmail.com>
2020-10-20 16:24:36 +0100

    Add basic high score table
    - Highscore functionality for adding new scores.
    - Displays only the top 5 scores and bins the rest.

---
+++ Assets/Scripts/AllLevels/HighScore/Scoreboard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using UnityEngine;
+
+namespace AllLevels.HighScore
+{
+    public class Scoreboard : MonoBehaviour
+    {
+        [SerializeField] private int maxScoreboardEntries = 5;
+        [SerializeField] private Transform highScoresHolderTransform = null;
+        [SerializeField] private GameObject scoreBoardEntryObject = null;
+        
+        [SerializeField] ScoreboardEntryData testEntryData = new ScoreboardEntryData();
+
+        private ScoreboardSaveData savedScores;
+        private string SavePath => $"{Application.persistentDataPath}/highscores.json";
+
+        private void Start()
+        {
+            savedScores = GetSavedScores();
+            SaveScores(savedScores);
+            UpdateUI(savedScores);
+        }
+        
+        [ContextMenu("Add Test Data")]
+        public void AddTestEntry()
+        {
+            AddEntry(testEntryData);
+        }
+
+        public void AddEntry(ScoreboardEntryData scoreboardEntryData)
+        {
+            savedScores = GetSavedScores();
+
+            bool scoreAdded = false;
+
+            for (int i = 0; i < savedScores.highscores.Count; i++)
+            {
+                if (scoreboardEntryData.entryScore > savedScores.highscores[i].entryScore)
+                {
+                    savedScores.highscores.Insert(i, scoreboardEntryData);
+                    scoreAdded = true;
+                    break;
+                }
+            }
+
+            if (!scoreAdded && savedScores.highscores.Count < maxScoreboardEntries)
+            {
+                savedScores.highscores.Add(scoreboardEntryData);
+            }
+
+            if (savedScores.highscores.Count > maxScoreboardEntries)
+            {
+                savedScores.highscores.RemoveRange(maxScoreboardEntries, 
+                    savedScores.highscores.Count - maxScoreboardEntries);
+            }
+            
+            UpdateUI(savedScores);
+
+            SaveScores(savedScores);
+        }
+
+        private void UpdateUI(ScoreboardSaveData scoreboardSaveData)
+        {
+            foreach (Transform child in highScoresHolderTransform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            foreach (ScoreboardEntryData highscore in savedScores.highscores)
+            {
+                Instantiate(scoreBoardEntryObject, highScoresHolderTransform).
+                    GetComponent<ScoreboardEntryUI>().Initialise(highscore);
+            }
+        }
+
+        private ScoreboardSaveData GetSavedScores()
+        {
+            if (!File.Exists(SavePath))
+            {
+                File.Create(SavePath).Dispose();
+                return new ScoreboardSaveData();
+            }
+            
+            using (StreamReader stream = new StreamReader(SavePath))
+            {
+                string json = stream.ReadToEnd();
+                return JsonUtility.FromJson<ScoreboardSaveData>(json);
+            }
+        }
+
+        private void SaveScores(ScoreboardSaveData scoreboardSaveData)
+        {
+            using (StreamWriter stream = new StreamWriter(SavePath))
+            {
+                string json = JsonUtility.ToJson(scoreboardSaveData, true);
+                stream.Write(json);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AllLevels/HighScore/ScoreboardEntryUI.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace AllLevels.HighScore
+{
+    public class ScoreboardEntryUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI entryNameText = null;
+        [SerializeField] private TextMeshProUGUI entryScoreText = null;
+
+        public void Initialise(ScoreboardEntryData scoreboardEntryData)
+        {
+            entryNameText.text = scoreboardEntryData.entryName;
+            entryScoreText.text = scoreboardEntryData.entryScore.ToString();
+        }
+    }
+}
